648b50185f39f8bc0879235f22065b6891547bc5
Alicia-vH <127492012+Alicia-vH@users.noreply.github.com>
2024-01-18 16:34:20 +0100

    Database migrations

---
+++ app/Models/Bestelling.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bestelling extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['date'];
+    public  $timestamps = false;
+}
