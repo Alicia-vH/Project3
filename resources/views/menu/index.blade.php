afbed57245155b71b20f80da1eac48a977fec012
Alicia-vH <127492012+Alicia-vH@users.noreply.github.com>
2024-01-31 14:05:31 +0100

    Website building

---
+++ resources/views/menu/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="NL">
+
+
+<head>
+    <title>Menu</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=julius-sans-one:400" rel="stylesheet" />
+</head>
+
+<header class="bg-pink h-50 text-center text-light text-5xl">Welcome to the menu page</header>
+
+</html>
