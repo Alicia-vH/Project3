e5441382ab3ecee7afca3e96d08766706ca9f672
Alicia-vH <127492012+Alicia-vH@users.noreply.github.com>
2024-01-31 23:49:05 +0100

    Website building

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 
 
 <head>
-    <title>Home</title>
+    <title>Stonks pizza</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
@@ -11,7 +11,20 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=julius-sans-one:400" rel="stylesheet" />
 </head>
+<div>
+    <header class="grid h-16 bg-red-600 border-green-800 border-solid border-4
+                        m-0 h-50 text-center text-light text-white text-4xl">Welkom bij Stonks Pizza!</header>
 
-<header class="h-16 rounded-sm bg-red-600 border-green-800 border-solid border-4 m-0 h-50 text-center text-light text-5xl">Welcome to the homepage</header>
+    <body class="bg-orange-200">
+        <div class="grid bg-red-600 border-green-800 border-solid border-4 absolute inset-y-0 right-0 w-20 h-auto">
+
+        </div>
+        <div class="grid bg-orange-600 border-red-600 border-solid border-4 w-3/4 h-80">
+
+        </div>
+    </body>
+
+    <footer class="grid h-16 bg-red-600 border-green-800 border-solid border-4 inset-x-0 bottom-0"></footer>
+</div>
 
 </html>
